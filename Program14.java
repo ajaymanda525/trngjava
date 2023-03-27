d74eb1195719bce3dbb837c2c0ed9919811f7f1a
ajaymanda525 <129029829+ajaymanda525@users.noreply.github.com>
2023-03-27 10:46:01 +0530

    Add files via upload

---
+++ Program14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Program14
+{
+public static void main(String args[]){
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();
+if (num %2==0){System.out.print("EVEN");}
+if (num %2 !=0){System.out.print("ODD"); }
+}
+}
