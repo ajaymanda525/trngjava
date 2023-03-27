24b5280d54d49c2584fa120295d6c87690af5956
ajaymanda525 <129029829+ajaymanda525@users.noreply.github.com>
2023-03-27 10:50:25 +0530

    Add files via upload

---
+++ Program16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Program16
+{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num==1){System.out.print("JAN");}
+else if (num==2){System.out.print("FEB");}
+else if (num==3){System.out.print("MAR");}
+else if (num==4){System.out.print("APR");}
+else if (num==5){System.out.print("MAY");}
+else if (num==6){System.out.print("JUNE");}
+else if (num==7){System.out.print("JULY");}
+else if (num==8){System.out.print("AUG");}
+else if (num==9){System.out.print("SEP");}
+else if (num==10){System.out.print("OCT");}
+else if (num==11){System.out.print("NOV");}
+else if (num==12){System.out.print("DEC");}
+else {System.out.print("INVALID");}
+}
+} 
