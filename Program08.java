80e97cf704738eaf0a303fa62789e2366946f2cb
ajaymanda525 <129029829+ajaymanda525@users.noreply.github.com>
2023-03-27 10:43:14 +0530

    Add files via upload

---
+++ Program08.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Program08
+{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int num1 =sc.nextInt();
+int i;
+i=0;
+while(i<=num1)
+{
+	System.out.println(i+ ",");
+	i=i+5;
+}
+}
+}
